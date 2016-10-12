f2a0a1869ab6ce4024819c204d21654b4ba8e886
SnehaMidha <sneha.midha@iic.ac.in>
2016-10-12 18:03:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,30 @@
 //console.log('Loaded!');
 //count code
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function()
 {
-  //make a request to the counter end point
-  
+  //create a request object
+  var request=new XMLHttpRequest()
   //Capture the response and store it in a variable
-  
-  
+  request.onreadystatechange=function(){
+    if(request.readyState===(XMLHttpRequest.DONE))
+    {
+       
+      //do some action
+      if(request.status===200)
+      {
+          var counter=request.responseText;
+          var span=document.getElementById('count');
+          span.innerHTML=counter.toString();
+      }
+  }
+    };
+    //make the request
+  request.open('GET','http:snehamidha.imad.hasura-app.io/counter',true);
+  request.send(null);
   //render the variable in correct span
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+  //counter=counter+1;
+  //var span=document.getElementById('count');
+  //span.innerHTML=counter.toString();
 };
